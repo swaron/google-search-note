6209bf454f5f02626b41f7a216d6cf7145fb04e5
swaron <swaron.chen@gmail.com>
2012-06-27 21:31:19 +0800

    update as tutorial.

--- greennet/code/content/browserOverlay.js
+++ greennet/code/content/browserOverlay.js
@@ -4,6 +4,9 @@
 if ("undefined" == typeof(GreenNetHelper)) {
   var GreenNetHelper = {};
 };
+if ("undefined" == typeof(XULSchoolChrome)) {
+	var XULSchoolChrome = {};
+};
 
 /**
  * Controls the browser overlay for the Hello World extension.
@@ -19,3 +22,18 @@ GreenNetHelper.BrowserOverlay = {
     window.alert(message);
   }
 };
+
+XULSchoolChrome.GreetingDialog = {
+		greetingShort : function(e){
+			window.alert("greeting from short");
+		},
+		greetingMedium:function(){
+			alert("greeting from medium");
+		},
+		greetingLong:function(){
+			window.alert("greeting from long");
+		},
+		greetingCustom:function(){
+			window.alert("greeting from custom");
+		}
+}
