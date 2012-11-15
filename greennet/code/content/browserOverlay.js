8dcf9fd06ea7699ee79177f80d2b4e5c11ff2d9a
swaron <swaron.chen@gmail.com>
2012-11-15 20:54:18 +0800

    to bad, the redirection function only write the destination url after page is loaded. so the redirection is some kind of faked. however, keeping the https scheme is still improve some stablity.

--- greennet/code/content/browserOverlay.js
+++ greennet/code/content/browserOverlay.js
@@ -33,11 +33,11 @@ GreenNetHelper.BrowserOverlay = {
 	}
 };
 
-GreenNetHelper.HttpReqObserver = function () {
+GreenNetHelper.HttpReqObserver = function() {
 	this.register();
 };
-(function(){
-	 var getWindowForRequest = function(request) {
+(function() {
+	var getWindowForRequest = function(request) {
 		if (request instanceof Components.interfaces.nsIRequest) {
 			try {
 				if (request.notificationCallbacks) {
@@ -54,7 +54,7 @@ GreenNetHelper.HttpReqObserver = function () {
 		}
 		return null;
 	};
-	var getQueryVariable = function (query,variable) {
+	var getQueryVariable = function(query, variable) {
 		var vars = query.split('&');
 		for (var i = 0; i < vars.length; i++) {
 			var pair = vars[i].split('=');
@@ -64,39 +64,138 @@ GreenNetHelper.HttpReqObserver = function () {
 		}
 		return null;
 	};
-GreenNetHelper.HttpReqObserver.prototype = {
+
+	function CtxCapturingListener(tracingChannel, channel) {
+		this.originalListener = tracingChannel.setNewListener(this);
+		this.channel = channel;
+	}
+	CtxCapturingListener.prototype = {
+		originalListener : null,
+		originalCtx : null,
+		setOriginalListener:function(originalListener){
+			this.originalListener = originalListener;
+		},
+		onStartRequest : function(request, ctx) {
+			this.originalCtx = ctx;
+			if (this.channel) {
+				this.channel.asyncOpen(this.originalListener,this.originalCtx);
+			}
+		},
+		onDataAvailable : function(request, ctx, inputStream, offset, count) {
+		},
+		onStopRequest : function(request, ctx, statusCode) {
+		},
+		QueryInterface : xpcom_generateQI([Ci.nsIStreamListener])
+	}
+
+	var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+	var HeaderCopy = function(channel) {
+		this.channel = channel;
+	}
+	HeaderCopy.prototype = {
+		channel : null,
+		_autoHeadersRx : /^(?:Host|Cookie|Authorization)$|Cache|^If-/,
+		visitHeader : function(key, val) {
+			try {
+				// we skip authorization and cache-related fields which should be automatically set
+				if (!this._autoHeadersRx.test(key)) {
+					this.channel.setRequestHeader(key, val, false);
+				}
+			} catch (e) {
+				dump(e + "\n");
+			}
+		}
+	}
+	var replaceChannel = function(channel, uri) {
+		var newChannel = ios.newChannelFromURI(uri);
+		if (!(newChannel instanceof Ci.nsIHttpChannel)) {
+			return false;
+		}
+		newChannel.loadGroup = channel.loadGroup;
+		newChannel.notificationCallbacks = channel.notificationCallbacks;
+		newChannel.loadFlags = channel.loadFlags | channel.LOAD_REPLACE;
+		channel.visitRequestHeaders(new HeaderCopy(newChannel));
+		if(channel.referrer){
+			newChannel.referrer = channel.referrer;
+		}
+		newChannel.allowPipelining = channel.allowPipelining;
+		
+		// Check for the new internal redirect API. If it exists, use it.
+		if ("redirectTo" in channel) {
+			try {
+				channel.redirectTo(uri);
+				return true;
+			} catch (e) {
+				// This should not happen. We should only get exceptions if
+				// the channel was already open.
+				log("Exception on nsIHttpChannel.redirectTo: " + e);
+				// Don't return: Fallback to NoScript ChannelReplacement.js
+			}
+		}
+		if (channel.status) {
+			return false; // channel's doom had been already defined
+		}
+		if (channel.isPending()) {
+			return false; // not clear redirect on peding channel is good or not.
+		}
+		
+		channel.QueryInterface(Components.interfaces.nsITraceableChannel);
+		
+		var newListener = new CtxCapturingListener(channel,newChannel);
+		channel.QueryInterface(Components.interfaces.nsIHttpChannel);
+//		channel.cancel(Components.results.NS_BINDING_REDIRECTED);
+		newChannel.QueryInterface(Components.interfaces.nsITraceableChannel);
+		newChannel.setNewListener(newListener.originalListener);
+//		newChannel.open();
+		return true;
+	};
+
+	GreenNetHelper.HttpReqObserver.prototype = {
 		observe : function(aSubject, aTopic, aData) {
 			if ("http-on-modify-request" == aTopic) {
 				var httpChannel = aSubject.QueryInterface(Components.interfaces.nsIHttpChannel);
-				if(httpChannel == null){
+				if (httpChannel == null) {
 					return;
 				}
 				var uri = httpChannel.URI;
 				var host = uri.host;
-				if(host.indexOf('google.com') == -1){
-					//not google site, ignore 
+				if (host.indexOf('google.com') == -1) {
+					// not google site, ignore
 					return;
 				}
-//				var win = getWindowForRequest(aSubject);
+				// var win = getWindowForRequest(aSubject);
 				var path = uri.path;
-				if(path.indexOf('/search?') == 0){
-					//this is a search action, change http to https. todo:filter keywords?
-					if(uri.schemeIs('http')){
+				if (path.length < 2 || path.indexOf('/?') == 0) {
+					// this is a search action, change http to https. todo:filter keywords?
+					if (uri.schemeIs('http')) {
 						uri.scheme = 'https';
 					}
-				}else if(path.indexOf('/url?') == 0){
+				} else if (path.indexOf('/search?') == 0) {
+					// this is a search action, change http to https. todo:filter keywords?
+					if (uri.schemeIs('http')) {
+						uri.scheme = 'https';
+					}
+				} else if (path.indexOf('/url?') == 0) {
 					var query = path.substr(5);
-					var url= getQueryVariable(query,'url');
-					if(url !=null && url.length > 3){
-						var ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+					var url = getQueryVariable(query, 'url');
+					if (url != null && url.length > 3) {
+						var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+								.getService(Components.interfaces.nsIIOService);
 						var newUri = ioService.newURI(url, "utf-8", null);
-						if(newUri.schemeIs(uri.scheme)){
+						if (newUri.schemeIs(uri.scheme)) {
 							uri.spec = url;
-						}else{
+						} else {
 							uri.spec = url;
 							uri.scheme = newUri.scheme;
 						}
 					}
+				}else{
+					return;
+				}
+				var result = replaceChannel(httpChannel, uri);
+				if(!result){
+					log("Aborting redirection " + channel.name + ".");
+					//channel.cancel(Components.results.NS_ERROR_ABORT);
 				}
 			}
 		},
@@ -110,16 +209,16 @@ GreenNetHelper.HttpReqObserver.prototype = {
 					.getService(Components.interfaces.nsIObserverService);
 			observerService.removeObserver(this, "http-on-modify-request");
 		}
-};
+	};
 
 })();
 
-if(GreenNet_Global.httpReqObserver == null){
+if (GreenNet_Global.httpReqObserver == null) {
 	GreenNet_Global.httpReqObserver = new GreenNetHelper.HttpReqObserver();
 }
 
 window.addEventListener("load", function() {
-	
+
 }, false);
 window.addEventListener("unload", function() {
 }, false);
--- greennet/code/content/common.js
+++ greennet/code/content/common.js
@@ -2,7 +2,7 @@ Components.utils.import("resource://gre/modules/FileUtils.jsm");
 Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
 if ("undefined" == typeof(GreenNetHelper)) {
-  var GreenNetHelper = {};
+	var GreenNetHelper = {};
 };
 if ("undefined" == typeof(Cc)) {
 	var Cc = Components.classes;
@@ -10,50 +10,58 @@ if ("undefined" == typeof(Cc)) {
 }
 
 function xpcom_generateQI(iids) {
-  iids.push(CI.nsISupports);
-  return function QueryInterface(iid) {
-	for (let i = 0, len = iids.length; i < len; i++)
-	  if (iids[i].equals(iid)) return this;
-	throw Components.results.NS_ERROR_NO_INTERFACE;
-  }
+	iids.push(Ci.nsISupports);
+	return function QueryInterface(iid) {
+		for (var i = 0, len = iids.length; i < len; i++)
+			if (iids[i].equals(iid))
+				return this;
+		throw Components.results.NS_ERROR_NO_INTERFACE;
+	}
 }
+
+function log(str) {
+	var econsole = Components.classes["@mozilla.org/consoleservice;1"]
+				.getService(Components.interfaces.nsIConsoleService);
+	econsole.logStringMessage("GreenNetHelper: " + str);
+}
+
 GreenNetHelper.apply = function(obj, extObj) {
-    if (arguments.length > 2) {
-        for (var a = 1; a < arguments.length; a++) {
-        	GreenNetHelper.apply(obj, arguments[a]);
-        }
-    } else {
-    	if(extObj != null){
-    		for (var i in extObj) {
-    			obj[i] = extObj[i];
-    		}
-    	}
-    }
-    return obj;
+	if (arguments.length > 2) {
+		for (var a = 1; a < arguments.length; a++) {
+			GreenNetHelper.apply(obj, arguments[a]);
+		}
+	} else {
+		if (extObj != null) {
+			for (var i in extObj) {
+				obj[i] = extObj[i];
+			}
+		}
+	}
+	return obj;
 };
 
 GreenNetHelper.apply(GreenNetHelper, {
 	getLocalDirectory : function() {
 		var localDir = FileUtils.getDir("ProfD", ["GreenNetHelper"], true);
-		if(!localDir.exists() || !localDir.isDirectory()){
-			Components.utils.reportError("common.js:error unreadable directory  " +localDir.path);
+		if (!localDir.exists() || !localDir.isDirectory()) {
+			Components.utils.reportError("common.js:error unreadable directory  " + localDir.path);
 		}
 		return localDir;
 	},
-	loadJsonFile : function(filename,callback){
-		if(typeof(callback) != 'function' ){
+	loadJsonFile : function(filename, callback) {
+		if (typeof(callback) != 'function') {
 			return;
 		}
-		var localDir = GreenNetHelper.getLocalDirectory(); 
-		var file = FileUtils.getFile("ProfD", ["GreenNetHelper",filename]);
-		if(!file.exists()){
+		var localDir = GreenNetHelper.getLocalDirectory();
+		var file = FileUtils.getFile("ProfD", ["GreenNetHelper", filename]);
+		if (!file.exists()) {
 			file.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
 			return;
 		}
 		var channel = NetUtil.newChannel(file);
 		channel.contentType = "application/json";
 		var config = {
-			lastUpdate: null,
+			lastUpdate : null,
 			version : null,
 			sites : [],
 			keywords : []
@@ -65,35 +73,36 @@ GreenNetHelper.apply(GreenNetHelper, {
 			}
 			// The file data is contained within inputStream.
 			// You can read it into a string with
-			var data = NetUtil.readInputStreamToString(inputStream,inputStream.available(),{
-				charset:'utf-8'
+			var data = NetUtil.readInputStreamToString(inputStream, inputStream.available(), {
+				charset : 'utf-8'
 			});
-			try{
+			try {
 				var content = JSON.parse(data);
-				//GreenNetHelper.apply(config,content);
+				// GreenNetHelper.apply(config,content);
 				callback(content);
-			}catch(e){
+			} catch (e) {
 				Components.utils.reportError("common.js:error on read json data.");
 			}
 		});
 	},
-	saveJsonFile : function(filename,content){
-		var file = FileUtils.getFile("ProfD", ["GreenNetHelper",filename]);
-		if(!file.exists()){
+	saveJsonFile : function(filename, content) {
+		var file = FileUtils.getFile("ProfD", ["GreenNetHelper", filename]);
+		if (!file.exists()) {
 			file.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
 		}
 		content.lastUpdate = Date.now();
 		var data = JSON.stringify(content);
-	  	var ostream = FileUtils.openSafeFileOutputStream(file);
-	  	var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
-	  	converter.charset = 'utf-8';
-	  	var istream = converter.convertToInputStream(data);
-	  	NetUtil.asyncCopy(istream,ostream,function(status){
-	  		if (!Components.isSuccessCode(status)) {
-	  			alert("falied to save json file.");
-		    	// Handle error!
-		    	return;
+		var ostream = FileUtils.openSafeFileOutputStream(file);
+		var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"]
+				.createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
+		converter.charset = 'utf-8';
+		var istream = converter.convertToInputStream(data);
+		NetUtil.asyncCopy(istream, ostream, function(status) {
+			if (!Components.isSuccessCode(status)) {
+				alert("falied to save json file.");
+				// Handle error!
+				return;
 			}
-	  	});
+		});
 	}
 });
