b9a7d68aa4521a1d6d39bceb1f36a3fd4e037398
swaron <swaron.chen@gmail.com>
2012-11-17 16:00:23 +0800

    Use WebProgressListeners instead of observe http-on-modify-request. it is now a really working version.

--- greennet/code/content/browserOverlay.js
+++ greennet/code/content/browserOverlay.js
@@ -1,42 +1,118 @@
-/**
- * GreenNetHelper namespace.
- */
-if ("undefined" == typeof(GreenNetHelper)) {
-	var GreenNetHelper = {};
-};
-
 Components.utils.import("resource://greennethelper/Global.js");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
 Components.utils.import("resource://gre/modules/NetUtil.jsm");
-/**
- * Controls the browser overlay for the Hello World extension.
- */
-GreenNetHelper.BrowserOverlay = {
-	init : function() {
-		var observerService = Components.classes["@mozilla.org/observer-service;1"]
-				.getService(Components.interfaces.nsIObserverService);
-	},
-	/**
-	 * Says 'Hello' to the user.
-	 */
-	sayHello : function(aEvent) {
-		var stringBundle = document.getElementById("greennethelper-string-bundle");
-		var message = stringBundle.getString("greennethelper.greeting.label");
-		window.openDialog('chrome://greennethelper/content/options.xul', "Preference of Green Net Helper",
-				'chrome,titlebar,toolbar,centerscreen,dialog=yes')
-		// window.open('chrome://greennethelper/content/someWindow.xul','some-window','chrome,centerscreen');
-	},
-	echo : function(aevent) {
-		var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"].getService(Ci.nsIPromptService);
-		prompts.alert(window, "Alert Title", "Popup Count: " + GreenNet_Global.openCount);
-		GreenNet_Global.openCount++;
-	}
-};
 
-GreenNetHelper.HttpReqObserver = function() {
-	this.register();
-};
 (function() {
+
+	function resolveUri(uri){
+		var host = uri.host;
+		if (host.indexOf('google.com') == -1) {
+			// not google site, ignore
+			return uri;
+		}
+		var path = uri.path;
+		if (path.length < 2 || path.indexOf('/?') == 0) {
+			//home page, not redirect to https now
+//			if (uri.schemeIs('http')) {
+//				uri.scheme = 'https';
+//			}
+		} else if (path.indexOf('/search?') == 0) {
+			// this is a search action, change http to https. todo:filter keywords?
+			if (uri.schemeIs('http')) {
+				var newUri = uri.clone();
+				newUri.scheme = 'https';
+				return newUri;
+			}
+		} else if (path.indexOf('/url?') == 0) {
+			if (uri.schemeIs('http')) {
+				var newUri = uri.clone();
+				newUri.scheme = 'https';
+				return newUri;
+			}
+//			var query = path.substr(5);
+//			var url = getQueryVariable(query, 'url');
+//			if (url != null && url.length > 3) {
+//				var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+//						.getService(Components.interfaces.nsIIOService);
+//				var newUri = ioService.newURI(url, "utf-8", null);
+//				return newUri;
+//			}
+		}
+		return uri;
+	}
+	var Gnh = GreenNetHelper;
+	var TabListener = function(){
+	};
+	const nsIWebProgressListener = Components.interfaces.nsIWebProgressListener;
+	TabListener.prototype = {
+		onLocationChange : function(aBrowser /* nsIDOMXULElement */, aWebProgress /* nsIWebProgress */,
+				aRequest /* nsIRequest */, aLocation /* nsIURI */
+		) {
+//			alert('on location change.' +aLocation.spec);
+		},
+		onProgressChange:function(aBrowser /* nsIDOMXULElement */, aWebProgress /* nsIWebProgress */,
+				aRequest /* nsIRequest */){
+//			alert('on Progress Change. ' +aRequest.name);
+		},
+		onSecurityChange:function(aBrowser /* nsIDOMXULElement */, aWebProgress /* nsIWebProgress */,
+				aRequest /* nsIRequest */){
+			//alert('on Security Change.');
+		},
+		onStateChange:function(aBrowser /* nsIDOMXULElement */, aWebProgress /* nsIWebProgress */,
+				aRequest /* nsIRequest */,aStateFlags){
+			if (aStateFlags & nsIWebProgressListener.STATE_IS_DOCUMENT
+	                    && aStateFlags & nsIWebProgressListener.STATE_START) {
+	            var httpChannel = aRequest.QueryInterface(Components.interfaces.nsIHttpChannel);
+	            if(httpChannel == null){
+	            	return;
+	            }
+	            var uri = httpChannel.URI;      	
+	            try {
+	            	alert("testing " + uri.spec);
+	                var resultUri = resolveUri(uri);
+	                if (resultUri != uri && resultUri.spec != uri.spec) {
+	                    if(resultUri == resolveUri(resultUri)){
+		                    aRequest.cancel(Components.results.NS_BINDING_REDIRECTED);
+	                    	//make sure the resolved uri will not be resolved to another.
+	                    	//if this case happend, loop will occur.
+		                    aWebProgress.DOMWindow.location.href = resultUri.spec;
+	                    }else{
+	                    	alert("there is a bug in resolve uri function.")
+	                    }
+	                }
+	            }
+	            catch(ex) {
+	            	alert("error on redirect:" +aRequest.name + '\n'+ ex);
+	            }
+	        }
+		},
+		onStatusChange:function(aBrowser /* nsIDOMXULElement */, aWebProgress /* nsIWebProgress */,
+				aRequest /* nsIRequest */,aStatus,aMessage){
+//			alert('on status Change. message: ' + aMessage);
+		},
+		onRefreshAttempted:function(aBrowser /* nsIDOMXULElement */, aWebProgress /* nsIWebProgress */,
+				aRefreshURI /* nsIURI */){
+//			alert('on Refresh Change. to new uri: ' + aRefreshURI);
+		},
+		QueryInterface: function (aIID) {
+	        if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+	                aIID.equals(Components.interfaces.nsIWebProgressListener2) ||
+	                aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+	                aIID.equals(Components.interfaces.nsISupports)) {
+	            return this;
+	        }
+	        else {
+	            throw Components.results.NS_NOINTERFACE;
+	        }
+    	}
+	};
+	
+	Gnh.TabProgressListener = TabListener;
+
+	GreenNetHelper.HttpReqObserver = function() {
+		this.register();
+	};
+	
 	var getWindowForRequest = function(request) {
 		if (request instanceof Components.interfaces.nsIRequest) {
 			try {
@@ -65,91 +141,6 @@ GreenNetHelper.HttpReqObserver = function() {
 		return null;
 	};
 
-	function CtxCapturingListener(tracingChannel, channel) {
-		this.originalListener = tracingChannel.setNewListener(this);
-		this.channel = channel;
-	}
-	CtxCapturingListener.prototype = {
-		originalListener : null,
-		originalCtx : null,
-		setOriginalListener:function(originalListener){
-			this.originalListener = originalListener;
-		},
-		onStartRequest : function(request, ctx) {
-			this.originalCtx = ctx;
-			if (this.channel) {
-				this.channel.asyncOpen(this.originalListener,this.originalCtx);
-			}
-		},
-		onDataAvailable : function(request, ctx, inputStream, offset, count) {
-		},
-		onStopRequest : function(request, ctx, statusCode) {
-		},
-		QueryInterface : xpcom_generateQI([Ci.nsIStreamListener])
-	}
-
-	var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
-	var HeaderCopy = function(channel) {
-		this.channel = channel;
-	}
-	HeaderCopy.prototype = {
-		channel : null,
-		_autoHeadersRx : /^(?:Host|Cookie|Authorization)$|Cache|^If-/,
-		visitHeader : function(key, val) {
-			try {
-				// we skip authorization and cache-related fields which should be automatically set
-				if (!this._autoHeadersRx.test(key)) {
-					this.channel.setRequestHeader(key, val, false);
-				}
-			} catch (e) {
-				dump(e + "\n");
-			}
-		}
-	}
-	var replaceChannel = function(channel, uri) {
-		var newChannel = ios.newChannelFromURI(uri);
-		if (!(newChannel instanceof Ci.nsIHttpChannel)) {
-			return false;
-		}
-		newChannel.loadGroup = channel.loadGroup;
-		newChannel.notificationCallbacks = channel.notificationCallbacks;
-		newChannel.loadFlags = channel.loadFlags | channel.LOAD_REPLACE;
-		channel.visitRequestHeaders(new HeaderCopy(newChannel));
-		if(channel.referrer){
-			newChannel.referrer = channel.referrer;
-		}
-		newChannel.allowPipelining = channel.allowPipelining;
-		
-		// Check for the new internal redirect API. If it exists, use it.
-		if ("redirectTo" in channel) {
-			try {
-				channel.redirectTo(uri);
-				return true;
-			} catch (e) {
-				// This should not happen. We should only get exceptions if
-				// the channel was already open.
-				log("Exception on nsIHttpChannel.redirectTo: " + e);
-				// Don't return: Fallback to NoScript ChannelReplacement.js
-			}
-		}
-		if (channel.status) {
-			return false; // channel's doom had been already defined
-		}
-		if (channel.isPending()) {
-			return false; // not clear redirect on peding channel is good or not.
-		}
-		
-		channel.QueryInterface(Components.interfaces.nsITraceableChannel);
-		
-		var newListener = new CtxCapturingListener(channel,newChannel);
-		channel.QueryInterface(Components.interfaces.nsIHttpChannel);
-//		channel.cancel(Components.results.NS_BINDING_REDIRECTED);
-		newChannel.QueryInterface(Components.interfaces.nsITraceableChannel);
-		newChannel.setNewListener(newListener.originalListener);
-//		newChannel.open();
-		return true;
-	};
-
 	GreenNetHelper.HttpReqObserver.prototype = {
 		observe : function(aSubject, aTopic, aData) {
 			if ("http-on-modify-request" == aTopic) {
@@ -163,21 +154,23 @@ GreenNetHelper.HttpReqObserver = function() {
 					// not google site, ignore
 					return;
 				}
-				// var win = getWindowForRequest(aSubject);
+				var win = getWindowForRequest(aSubject);
 				var path = uri.path;
 				if (path.length < 2 || path.indexOf('/?') == 0) {
-					// this is a search action, change http to https. todo:filter keywords?
-					if (uri.schemeIs('http')) {
-						uri.scheme = 'https';
-					}
+					//home page
+//					if (uri.schemeIs('http')) {
+//						uri.scheme = 'https';
+//					}
 				} else if (path.indexOf('/search?') == 0) {
 					// this is a search action, change http to https. todo:filter keywords?
 					if (uri.schemeIs('http')) {
-						uri.scheme = 'https';
+						var newUri = uri.clone();
+						newUri.scheme = 'https';
 					}
 				} else if (path.indexOf('/url?') == 0) {
 					if (uri.schemeIs('http')) {
-						uri.scheme = 'https';
+						var newUri = uri.clone();
+						newUri.scheme = 'https';
 					}
 //					var query = path.substr(5);
 //					var url = getQueryVariable(query, 'url');
@@ -209,15 +202,17 @@ GreenNetHelper.HttpReqObserver = function() {
 			observerService.removeObserver(this, "http-on-modify-request");
 		}
 	};
-
+	
 })();
 
 if (GreenNet_Global.httpReqObserver == null) {
-	GreenNet_Global.httpReqObserver = new GreenNetHelper.HttpReqObserver();
+//	GreenNet_Global.httpReqObserver = new GreenNetHelper.HttpReqObserver();
 }
 
+var listener = new GreenNetHelper.TabProgressListener();
 window.addEventListener("load", function() {
-
+	gBrowser.addTabsProgressListener(listener);
 }, false);
 window.addEventListener("unload", function() {
+	gBrowser.removeTabsProgressListener(listener);
 }, false);
